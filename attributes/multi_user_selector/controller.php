1252d9039ab5e701e60e21d895f446286b1fae5d
a3020 <a3020@fastmail.com>
2016-11-21 15:50:38 +0100

    Initial commit

---
+++ attributes/multi_user_selector/controller.php
@@ -0,0 +1,106 @@
+<?php
+namespace Concrete\Package\MultiUserSelectorAttribute\Attribute\MultiUserSelector;
+
+use Concrete\Core\Attribute\Controller as CoreController;
+use Core;
+use Database;
+
+class Controller extends CoreController
+{
+	public function getRawValue()
+	{
+		$db = Database::connection();
+		$value = $db->fetchColumn("SELECT value FROM atMultiUserSelector WHERE avID = ?", [
+			$this->getAttributeValueID()
+		]);
+
+		return trim($value);
+	}
+
+	public function getValue() : array
+	{
+		$value = $this->getRawValue() ?? "";
+		$users = [];
+
+		$ids = explode(',', $value);
+
+		foreach ($ids as $uID) {
+			$ui = Core::make(\Concrete\Core\User\UserInfoFactory::class)->getByID($uID);
+			if ($ui) {
+				$users[] = $ui;
+			}
+		}
+
+		return $users;
+	}
+
+	public function form()
+	{
+		$this->load();
+		$values = [];
+
+		if (is_object($this->attributeValue)) {
+			$values = $this->getAttributeValue()->getValue();
+		}
+
+		$us = Core::make('helper/form/user_selector');
+
+		echo $us->selectUser($this->field('value'), array_shift($values));
+	}
+
+	protected function load()
+	{
+		$ak = $this->getAttributeKey();
+		if (!is_object($ak)) {
+			return false;
+		}
+	}
+
+	public function saveValue($value)
+	{
+		$db = Database::connection();
+
+		if (is_array($value)) {
+			$value = implode(',', $value);
+		}
+
+		if (!$value) {
+			$value = '';
+		}
+
+		$db->Replace('atMultiUserSelector', [
+			'avID' => $this->getAttributeValueID(),
+			'value' => $value
+		], 'avID', true);
+	}
+
+	public function saveKey($data)
+	{
+		$db = Database::connection();
+		$ak = $this->getAttributeKey();
+	}
+
+	public function deleteKey()
+	{
+		$db = Database::connection();
+		$arr = $this->attributeKey->getAttributeValueIDList();
+		foreach ($arr as $id) {
+			$db->query("DELETE FROM atMultiUserSelector WHERE avID = ?", [
+				$id
+			]);
+		}
+	}
+
+	public function saveForm($data)
+	{
+		$this->saveValue($data['value']);
+	}
+
+	public function deleteValue()
+	{
+		$db = Database::connection();
+		$db->query("DELETE FROM atMultiUserSelector where avID = ?", [
+			$this->getAttributeValueID()
+		]);
+	}
+}
---
+++ controller.php
@@ -0,0 +1,36 @@
+<?php
+namespace Concrete\Package\MultiUserSelectorAttribute;
+
+use Concrete\Core\Package\Package;
+use Concrete\Core\Attribute\Key\Category as AttributeKeyCategory;
+use Concrete\Core\Attribute\Type as AttributeType;
+
+class Controller extends Package
+{
+    protected $pkgHandle = 'multi_user_selector_attribute';
+    protected $appVersionRequired = '5.7.5';
+    protected $pkgVersion = '0.9.0';
+
+    public function getPackageName()
+    {
+        return t('Multi User Selector Attribute');
+    }
+
+    public function getPackageDescription()
+    {
+        return t('Link one or more users to an entity');
+    }
+
+    public function install()
+    {
+        $pkg = parent::install();
+
+        $at = AttributeType::add('multi_user_selector', t('Multi User Selector'), $pkg);
+
+        $col = AttributeKeyCategory::getByHandle('collection');
+        $col->associateAttributeKeyType($at);
+
+        $col = AttributeKeyCategory::getByHandle('file');
+        $col->associateAttributeKeyType($at);
+    }
+}
